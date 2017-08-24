494d9f7b3a8d52355ce7aa70b6eb0656bb29a17a
Haoyu Li <lihaoyu2678@gmail.com>
2017-08-24 00:09:45 -0700

    first commit

---
+++ 99_bottles_of_beer_CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _bottles_of_beer_CSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
