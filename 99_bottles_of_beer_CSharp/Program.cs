d3888e3fb3fcef99815e966b5937a86847fb0976
Sandylee33 <lihaoyu2678@yahoo.com>
2017-08-24 00:14:06 -0700

    Update Program.cs

--- 99_bottles_of_beer_CSharp/Program.cs
+++ 99_bottles_of_beer_CSharp/Program.cs
@@ -9,13 +9,13 @@ namespace _bottles_of_beer_CSharp
             int n = 99;
             while (n > 1) {
                 Console.WriteLine(n + " bottles of beer on the wall, " + n + " bottles of beer.");
-				Console.WriteLine("Take one down and pass it around, " + (n - 1).ToString() + " bottles of beer on the wall.\n");
-                n = n - 1;
+		Console.WriteLine("Take one down and pass it around, " + (n - 1).ToString() + " bottles of beer on the wall.\n");
+                n --;
             }
             if (n == 1){
                 Console.WriteLine(n + " bottle of beer on the wall, " + n + " bottle of beer.");
-				 Console.WriteLine("Take one down and pass it around, " +  "no more bottles of beer on the wall.\n");
-                n = n - 1;
+		Console.WriteLine("Take one down and pass it around, " +  "no more bottles of beer on the wall.\n");
+                n --;
             }
             if (n == 0){
                 n = 99;
